571239b4a8a19937e5331e4074d83f353ae9af17
Delia Bakhouche <delia.bakhouche@gmail.com>
2016-07-04 22:07:36 +0200

    preparation environnement chai + mocha

---
+++ gestionnaireArticleTests/test/gestionnairearticle.js
@@ -0,0 +1,10 @@
+var expect    = require("chai").expect;
+var gestionnairearticle = require("../app.js");
+
+describe("Gestionnaire article", function() {
+  describe("test simple addition", function() {
+    it("add numbers", function() {
+      expect(1+1).to.equal(2);
+    });
+  });
+});

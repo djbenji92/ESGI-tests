568c30c58be936d43be15ebc6ddae4fcd8fbd370
Delia Bakhouche <delia.bakhouche@gmail.com>
2016-07-05 23:45:06 +0200

    nettoyage dossier plus test statusCode 200

--- gestionnaireArticleTests/test/gestionnairearticle.js
+++ gestionnaireArticleTests/test/gestionnairearticle.js
@@ -1,10 +1,19 @@
 var expect    = require("chai").expect;
-var gestionnairearticle = require("../app.js");
+var request = require("request");
+var url = "http://localhost:8080/#/articles";
 
 describe("Gestionnaire article", function() {
   describe("test simple addition", function() {
     it("add numbers", function() {
       expect(1+1).to.equal(2);
+    });
+  });
+  
+  describe("get all article", function() {
+    it("returns status 200", function() {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+      });
     });
   });
 });
